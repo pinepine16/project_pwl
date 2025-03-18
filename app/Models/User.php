92f50ef814590bc85ae5ca6887d270a113acdf36
pinepine <2372067@maranatha.ac.id>
2025-03-18 16:45:42 +0700

    progress 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'nim',
         'password',
+        'role_id'
     ];
 
     /**
